6dc4b7655d9d1b370898b56e65a458cd455d3e2c
meurglys <24libras@gmail.com>
2020-12-14 21:04:50 +0300

    Create rule_based_alienbot.py
    The 1950s ushered in a slew of UFO sightings and excitement about outer space. Incidentally, the decade also brought about a new idea of artificial intelligence. The computer scientist Alan Turing proposed that if a computer was able to fool a human into believing it was human, the computer would be considered “intelligent.”
    
    While your bot may not pass a true Turing test, perhaps it could pass an “E.T.uring” test if you can successfully fool a human into believing that your chatbot is, in fact, an alien.

---
+++ rule_based_alienbot.py
@@ -0,0 +1,133 @@
+# importing regex and random libraries
+import re
+import random
+
+class AlienBot:
+  # potential negative responses
+  negative_responses = ("no", "nope", "nah", "naw", "not a chance", "sorry", "stop")
+  # keywords for exiting the conversation
+  exit_commands = ("quit", "pause", "exit", "goodbye", "bye", "later")
+  # random starter questions
+  random_questions = (
+        "Why are you here? ",
+        "Are there many humans like you? ",
+        "What do you consume for sustenance? ",
+        "Is there intelligent life on this planet? ",
+        "Does Earth have a leader? ",
+        "What planets have you visited? ",
+        "What technology do you have on this planet? "
+    )
+
+  def __init__(self):
+    self.alienbabble = {'describe_planet_intent': r'.*\s*your planet.*',
+                        'answer_why_intent': r'.*why\s+.*',
+                        'cubed_intent': r'.*cube.*(\d+)'
+                            }
+
+  # Define .greet() below:
+  def greet(self):
+    self.name = input("Hello there, what's your name? ")
+    will_help = input(f"Hi {self.name}, I'm Etcetera. I'm not from this planet. Will you help me learn about your planet? ")
+
+    if will_help in self.negative_responses:
+      print("Ok, have a nice Earth day!")
+      return
+    self.chat()
+
+  # Define .make_exit() here:
+  def make_exit(self, reply):
+    for exit_command in self.exit_commands:
+      if exit_command in reply:
+        print("Ok, have a nice Earth day!")
+        return True
+    return False
+
+  # Define .chat() next:
+  def chat(self):
+    reply = input(random.choice(self.random_questions)).lower()
+
+    while not self.make_exit(reply):
+      reply = input(self.match_reply(reply))
+
+  # Define .match_reply() below:
+  def match_reply(self, reply):
+    for intent, regex_pattern in self.alienbabble.items():
+      found_match = re.match(regex_pattern, reply)
+      if found_match and intent == 'describe_planet_intent':
+        return self.describe_planet_intent()
+      elif found_match and intent == 'answer_why_intent':
+        return self.answer_why_intent()
+      elif found_match and intent == 'cubed_intent':
+        return self.cubed_intent(found_match.group()[-1])
+    else:
+      return self.no_match_intent()
+
+  # Define .describe_planet_intent():
+  def describe_planet_intent(self):
+    responses = (
+      "My planet is a utopia of diverse organisms and species. ",
+      "I am from Opidipus, the capital of the Wayward Galaxies. "
+    )
+    return random.choice(responses)
+
+  # Define .answer_why_intent():
+  def answer_why_intent(self):
+    responses = (
+      "I come in peace. ",
+      "I am here to collect data on your planet and its inhabitants. ",
+      "I heard the coffee is good. "
+    )
+    return random.choice(responses)
+       
+  # Define .cubed_intent():
+  def cubed_intent(self, num): 
+    num = int(num)
+    cubed_num = num ** 3
+    return (f"The cube of {num} is {cubed_num}. Isn't that cool? ")
+
+  # Define .no_match_intent():
+
+  def no_match_intent(self):
+    responses = (
+      "Please tell me more. ",
+      "Tell me more! ",
+      "Why do you say that? ",
+      "I see. Can you elaborate? ",
+      "Interesting. Can you tell me more? ",
+      "I see. How do you think? ",
+      "Why? ",
+      "How do you think I feel when you say that? "
+    )
+    return random.choice(responses)
+
+  #add reflections
+  reflections = {
+    "i'm": "you are",
+    "you're": "i'm",
+    "was": "were",
+    "i": "you",
+    "are": "am",
+    "am": "are",
+    "i'd": "you would",
+    "i've": "you have",
+    "i'll": "you will",
+    "my": "your",
+    "you've": "I have",
+    "you'll": "I will",
+    "your": "my",
+    "yours": "mine",
+    "you": "I",
+    "me": "you"
+    }
+  
+  # define reflect method:
+  def reflect(response):
+    words = response.split()
+    for index, word in enumerate(words):
+      if word in reflections:
+        words[index] = reflections[word]
+    return ' '.join(words)
+
+# Create an instance of AlienBot below:
+my_bot = AlienBot()
+my_bot.greet()
